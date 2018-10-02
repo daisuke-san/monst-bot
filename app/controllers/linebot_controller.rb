0bdd05993688a7f5247156996bed5b0c86ad90dc
Daisuke <daisuke@tashirodaisukenoMacBook.local>
2018-10-03 01:26:05 +0900

    linebot infra commit

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,5 +1,8 @@
 class LinebotController < ApplicationController
   require 'line/bot'  # gem 'line-bot-api'
+  require 'nokogiri'
+  require 'open-uri'
+  require 'sanitize'
 
   # callbackアクションのCSRFトークン認証を無効
   protect_from_forgery :except => [:callback]
@@ -26,16 +29,48 @@ class LinebotController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
-          puts "#{message}"
-          client.reply_message(event['replyToken'], message)
+          # message = {
+          #   type: 'text',
+          #   text: event.message['text']
+          # }
+          # client.reply_message(event['replyToken'], message)
         end
       end
     }
-
     head :ok
+  end
+
+  def scraiping
+    html_monst = open('https://xn--eckwa2aa3a9c8j8bve9d.gamewith.jp/')
+    doc = Nokogiri::HTML.parse(html_monst)
+    doc1 = doc.css('.monst_schedule_table')[0]
+    row = []
+    col1 = nil
+    col2 = nil
+    col3 = nil
+    col4 = nil
+
+    doc1.css('table tr td').each_with_index do |link, i|
+      i += 1
+      if i % 4 == 1
+        col1 = link.content
+      elsif i % 4 == 2
+        col2 = link.content
+      elsif i % 4 == 3
+        col3 = link.content
+      else
+        col4 = link.content
+        row << [col1, col2, col3, col4]
+     end
+    end
+
+    row.each do |_col1, _col2, _col3, _col4|
+      puts "#{_col1} : #{_col2}"
+      message = {
+        type: 'text',
+        text: "#{_col1} : #{_col2}\n"
+      }
+      client.reply_message(event['replyToken'], message)
+    end
   end
 end
