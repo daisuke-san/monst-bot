db6dd9deb14353126cc480a266b778f85918da46
Daisuke <daisuke@tashirodaisukenoMacBook.local>
2018-10-03 01:29:15 +0900

    linebot infra commit

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -14,33 +14,6 @@ class LinebotController < ApplicationController
     }
   end
 
-  def callback
-    body = request.body.read
-
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      error 400 do 'Bad Request' end
-    end
-
-    events = client.parse_events_from(body)
-
-    events.each { |event|
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          scraiping
-          # message = {
-          #   type: 'text',
-          #   text: event.message['text']
-          # }
-          # client.reply_message(event['replyToken'], message)
-        end
-      end
-    }
-    head :ok
-  end
-
   def scraiping
     html_monst = open('https://xn--eckwa2aa3a9c8j8bve9d.gamewith.jp/')
     doc = Nokogiri::HTML.parse(html_monst)
@@ -74,4 +47,32 @@ class LinebotController < ApplicationController
       client.reply_message(event['replyToken'], message)
     end
   end
+
+  def callback
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+
+    events = client.parse_events_from(body)
+
+    events.each { |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          scraiping
+          # message = {
+          #   type: 'text',
+          #   text: event.message['text']
+          # }
+          # client.reply_message(event['replyToken'], message)
+        end
+      end
+    }
+    head :ok
+  end
+
 end
