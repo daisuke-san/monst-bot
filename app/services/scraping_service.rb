bd404847efead33073e16a0ca3cc8bf03674e01e
Daisuke <daisuke@tashirodaisukenoMacBook.local>
2018-10-06 19:54:54 +0900

    create event schedule

--- app/services/scraping_service.rb
+++ app/services/scraping_service.rb
@@ -23,34 +23,50 @@ class ScrapingService
 
   # イベント情報取得メソッド
   def get_event_schedule
-      html_monst = open(MONST_URL)
-      doc = Nokogiri::HTML.parse(html_monst)
-      doc1 = doc.css('.monst_schedule_table')[0]
-      row = []
-      col1 = nil
-      col2 = nil
-      col3 = nil
-      col4 = nil
+    html_monst = open(MONST_URL)
+    doc = Nokogiri::HTML.parse(html_monst)
+    tables = doc.css('.monst_schedule_table')[0]
+    row = []
+    col1 = nil
+    col2 = nil
+    col3 = nil
+    col4 = nil
 
-      doc1.css('table tr td').each_with_index do |link, i|
-        i += 1
-        if i % 4 == 1
-          col1 = link.content
-        elsif i % 4 == 2
-          col2 = link.content
-        elsif i % 4 == 3
-          col3 = link.content
-        else
-          col4 = link.content
-          row << [col1, col2, col3, col4]
-       end
-      end
+    tables.css('table tr').each_with_index do |table|
+      headers = table.css('th')
+      dtls = table.css('td')
 
-      puts row
-      str = ""
-      row.each do |_col1, _col2, _col3, _col4|
-        str = str << "#{_col1} : #{_col2}\n"
+      if headers.empty?
+        dtls.each_with_index do |dtl, d_index|
+          d_index += 1
+          if d_index % 4 == 1
+            col1 = dtl.content
+          elsif d_index % 4 == 2
+            col2 = dtl.content
+          elsif d_index % 4 == 3
+            col3 = dtl.content
+          else
+            col4 = dtl.content
+            row << [col1, col2, col3, col4]
+          end
       end
-      return str
+      else
+        headers.each_with_index do |header, h_index|
+          h_index += 1
+          if h_index % 4 == 1
+            col1 = header.content
+            row << [col1]
+          end
+        end
+      end
+    end
+
+    str_message = ""
+    row.each do |_col1, _col2, _col3, _col4|
+      str_message = str_message << "#{_col1} : #{_col2}\n"
+    end
+
+    return str_message
   end
+
 end
