98b08a3cfa01afb0b732cbdfc91587748948c5f2
Daisuke <daisuke@tashirodaisukenoMacBook.local>
2018-10-08 00:23:04 +0900

    create quest guide fanc

--- app/services/scraping_service.rb
+++ app/services/scraping_service.rb
@@ -18,6 +18,42 @@ class ScrapingService
           }
         ]
       return message
+    elsif line_message.include?("禁忌")
+      # get_quest_guide
+      message = {
+        "type": "template",
+        "altText": "this is a image carousel template",
+        "template": {
+            "type": "image_carousel",
+            "columns": [
+                {
+                  "imageUrl": "https://example.com/bot/images/item1.jpg",
+                  "action": {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=111"
+                  }
+                },
+                {
+                  "imageUrl": "https://example.com/bot/images/item2.jpg",
+                  "action": {
+                    "type": "message",
+                    "label": "Yes",
+                    "text": "yes"
+                  }
+                },
+                {
+                  "imageUrl": "https://example.com/bot/images/item3.jpg",
+                  "action": {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/222"
+                  }
+                }
+            ]
+        }
+      }
+      return message
     else
       message = {
         type: 'text',
